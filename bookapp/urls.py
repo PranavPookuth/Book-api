e0a70995533cce126f043ee18ee05383370dc7d6
PranavPookuth <pranavpookuth@gmail.com>
2024-11-04 21:14:46 +0530

    PranavPookuth/Book-api

--- bookapp/urls.py
+++ bookapp/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='user-register'),
     path('verify-otp/', OTPVerifyView.as_view(), name='verify-otp'),
     path('login/', LoginView.as_view(), name='login'),
-    path('user/<int:pk>/',Userdetails.as_view(),name='user'),
+    path('user/<int:pk>/',Userdetails.as_view(),name='user') ,
     path('logout/', views.LogoutView.as_view(), name='logout'),
 
     path('password-reset/', views.PasswordResetView.as_view(), name='password-reset'),
